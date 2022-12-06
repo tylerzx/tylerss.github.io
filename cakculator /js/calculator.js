789e6ff1e94c93aa01ee4d704e3975c7590e2dea
tylerzx <115659545+tylerzx@users.noreply.github.com>
2022-12-06 11:56:49 -0600

    hi

--- cakculator /js/calculator.js
+++ cakculator /js/calculator.js
@@ -1,3 +1,10 @@
-{
-    
-}
+
+    function chooseLargerNumber(num1, num2) {
+        let larger;
+        if(num1 >= num1){
+            larger = num1;
+        } else {
+            Larger = num2;
+        }
+        return larger;      
+    }
---
+++ lightbox/lightbox.js
@@ -0,0 +1,15 @@
+let ligtboxContainer = document.
+
+getElementById("lightboxxontainter");
+
+let lightboxbackground = document.
+
+getElementById('lightboxbackground');
+
+let lightbox = document.getElementById
+
+('lightboxCloser');
+
+function openlightbox() {
+    
+    ligtboxContainer
